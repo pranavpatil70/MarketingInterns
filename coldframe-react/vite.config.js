9fed4042cb3be4298719b016f4ab78cd39a25e9c
vinu4545 <vinurocky1131@gmail.com>
2026-05-13 14:37:10 +0530

    Save local changes

--- coldframe-react/vite.config.js
+++ coldframe-react/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false,
+  },
+  server: {
+    port: 3000,
+  },
 })
